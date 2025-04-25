79250d553e5c6a6dde981fe72e10478da3d207d9
nkusi98stella <nkusistella19@gmail.com>
2025-04-25 12:49:03 +0200

    Add files via upload
    php_assignment submission

---
+++ check.php
@@ -0,0 +1,4 @@
+<?php
+echo ("The SHA256 hash of 'check.php' is: ");
+echo hash('sha256', 'check.php');
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Group_7 Assignment</title>
+</head>
+<body>
+    <h1>Group_7 Assignment</h1>
+    <p>SHA256 hash of "Group_7 Assignment" is:<br>
+    <?php
+    print hash('sha256', 'Group_7 Assignment');
+    ?>
+    </p>
+    <pre>
+        <p>ASCII ART:<br>
+        GGGGG
+       G
+       G  GGG
+        GGGG
+    </p>
+    </pre>
+    <a href="fail.php">Click here the file page</a><br>
+    <a href="check.php">Click here the check page</a>    
+</body>
+</html>
